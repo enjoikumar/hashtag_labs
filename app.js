21526b9adefac216491f1210ae27bbe9ce42091b
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2016-12-08 17:06:59 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// var myVar = setInterval(function(){ setColor() }, 300);
+
+// function setColor() {
+//     var x = document.body;
+//     x.style.backgroundColor = x.style.backgroundColor == "yellow" ? "pink" : "yellow";
+// }
+
+// function stopColor() {
+//     clearInterval(myVar);
+// }
